cc1b468174f11d9a20f588ca6a6c5f069e090068
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-24 13:10:47 +0530

    feat: route Developer challenge through session director

--- flutter_app/lib/games/flame_mini_game_factory.dart
+++ flutter_app/lib/games/flame_mini_game_factory.dart
@@ -1,8 +1,12 @@
+import '../models/developer_session_model.dart';
 import '../models/flame_mini_game_model.dart';
+import '../services/developer_session_director_service.dart';
+import '../services/progress_service.dart';
 import 'base_mini_game.dart';
 import 'blueprint_safety_puzzle_game.dart';
 import 'bug_hunt_room_game.dart';
 import 'data_cleanup_race_game.dart';
+import 'developer_release_pipeline_game.dart';
 
 class FlameMiniGameFactory {
   const FlameMiniGameFactory._();
@@ -17,11 +21,30 @@ class FlameMiniGameFactory {
   static BaseMiniGame create(FlameMiniGameKind kind) {
     switch (kind) {
       case FlameMiniGameKind.bugHuntRoom:
-        return BugHuntRoomGame();
+        final plan = DeveloperSessionDirectorService.instance.nextPlan(
+          history: ProgressService.instance.flameMiniGameHistory.value,
+        );
+        return createDeveloperPlan(plan);
       case FlameMiniGameKind.dataCleanupRace:
         return DataCleanupRaceGame();
       case FlameMiniGameKind.blueprintSafetyPuzzle:
         return BlueprintSafetyPuzzleGame();
+    }
+  }
+
+  static BaseMiniGame createDeveloperPlan(DeveloperSessionPlan plan) {
+    switch (plan.family) {
+      case DeveloperTaskFamily.liveIncident:
+        return BugHuntRoomGame(sessionPlan: plan);
+      case DeveloperTaskFamily.releasePipeline:
+        return DeveloperReleasePipelineGame(sessionPlan: plan);
+      case DeveloperTaskFamily.supportEscalation:
+      case DeveloperTaskFamily.performanceProfiling:
+        throw ArgumentError.value(
+          plan.family,
+          'plan.family',
+          'Developer task family is not playable yet.',
+        );
     }
   }
 
